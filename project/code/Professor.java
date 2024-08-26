b94c2d8d69f73cfd82109e691df81dd5728a1266
Gustavo Menezes Barbosa <gustavomb1313@gmail.com>
2024-08-26 15:11:19 -0300

    Classe Professores

--- project/code/Professor.java
+++ project/code/Professor.java
@@ -1,11 +1,20 @@
-import java.util.List;
-
 public class Professor extends Usuario{
 
     
-    public void consultarMatriculas(Turma turma){
-        List<Aluno> aluno = turma.getAlunos();
+    public Professor(String login, String senha) {
+        super(login, senha);
     }
 
+    public void consultarMatriculas(Turma turma){
+        Aluno[] alunos = turma.getAlunos();
 
+        if(alunos.length < 0){
+            System.out.println("Não possui alunos matriculados nessa diciplina");
+        }else{
+            System.out.println("Alunos matriculados na diciplina: " + turma.getDisciplina().getNome() + ": ");
+            for(Aluno aluno : alunos){
+                System.out.println(aluno.getLogin());
+            }
+        }
+    }
 }
