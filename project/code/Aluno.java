1db7a8e93e08b2933f180f23570dacb15c029229
Milena Lara <milenalrf@gmail.com>
2024-08-28 12:47:44 -0300

    Deleted unused imports and lerMatricula() on Aluno
    Class GerenciadorDeArquivos implements a file reader that can be used on every class, making the function lerMatricula() redundant.

--- project/code/Aluno.java
+++ project/code/Aluno.java
@@ -1,39 +1,5 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class Aluno extends Usuario{
     private Disciplina[] disciplinas;
-
-    public void lerMatriculas() throws FileNotFoundException{
-
-        Scanner scan = new Scanner(new File("project/code/Matriculas.txt"));
-
-        // registro de cada aluno é uma linha
-        // dados separados por tab (\t)
-        // chave e valor separados por ":"
-        // exemplo: login:valor\
- disciplina1:nome    disciplina2:nome  disciplinaN:nome 
-        while(scan.hasNextLine()){
-
-            String[] line = scan.nextLine().split("\t");
-
-            String[] loginStr = line[0].split(":");
-            this.setLogin(loginStr[0]);
-
-            for (int i = 1; i < line.length; i++) {
-                String[] str = line[0].split(":");
-            }
-        }
-
-        scan.close();
-    }
-
-
-
 
     public void matricularEmDisciplina(Disciplina disciplina){
         
